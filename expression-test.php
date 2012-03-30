be0d46e3bc0fb4777ff31adea8c25ac820e9a09e
Kim Henriksen <kh@ipimp.at>
2012-03-30 02:25:48 +0200

    Added try catches

--- expression-test.php
+++ expression-test.php
@@ -1,45 +1,65 @@
 <?php
 
 require 'pcapFilterExpression.php';
-$pcap_filter_expr = new pcapFilterExpression();
+$expr = new pcapFilterExpression();
 
 
 // Simple test
-$pcap_filter_expr->init(); // Init class
-$pcap_filter_expr->begin() 
-        ->host('192.168.0.2')
-        ->concate()
-        ->port(5060)
-        ->end();
+try {
+    // initialize class
+    $expr->init();
+    // compile expression
+    $expr->begin()
+            ->host('192.168.0.2')
+            ->concate()
+            ->port(5060)
+            ->end();
+    // Echo the expression
+    echo $expr->getPcapFilterExpressionString() . "\r\n";
+} catch (InvalidArgumentException $e) {
+    trigger_error($e->getMessage(), E_USER_ERROR);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
-// Echo the expression
-echo $pcap_filter_expr->getPcapFilterExpressionString(). "\r\n";
-
-// Port range
-$pcap_filter_expr->init(); // Init class
-$pcap_filter_expr->begin() 
-        ->host('192.168.0.2')
-        ->concate()
-        ->port_range(5060, 5061)
-        ->end();
-
-// Echo the expression
-echo $pcap_filter_expr->getPcapFilterExpressionString(). "\r\n";
-
+// Port range test
+try {
+    // initialize class
+    $expr->init();
+    // compile expression
+    $expr->begin()
+            ->host('192.168.0.2')
+            ->concate()
+            ->port_range(5060, 5061)
+            ->end();
+    // Echo the expression
+    echo $expr->getPcapFilterExpressionString() . "\r\n";
+} catch (InvalidArgumentException $e) {
+    trigger_error($e->getMessage(), E_USER_ERROR);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 
 // Two host test
-$pcap_filter_expr->init(); // Init class
-$pcap_filter_expr->begin() 
-        ->host('192.168.0.2')
-        ->alternate()
-        ->host('192.168.0.3')
-        ->group()
-        ->concate()
-        ->port(5060)
-        ->end();
-
-// Echo the expression
-echo $pcap_filter_expr->getPcapFilterExpressionString() . "\r\n";
-
-
+try {
+    // initialize class
+    $expr->init();
+    // compile expression
+    $expr->begin()
+            ->host('192.168.0.2')
+            ->alternate()
+            ->host('192.168.0.3')
+            ->group()
+            ->concate()
+            ->port(5060)
+            ->end();
+    // Echo the expression
+    echo $expr->getPcapFilterExpressionString() . "\r\n";
+} catch (Exception $e) {
+    echo $e->getMessage();
+} catch (InvalidArgumentException $e) {
+    trigger_error($e->getMessage(), E_USER_ERROR);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 ?>
